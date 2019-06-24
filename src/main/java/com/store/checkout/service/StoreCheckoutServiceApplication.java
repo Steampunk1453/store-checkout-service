62efee8d95b0cd7652b85bfced1a91d40116c872
Steampunk1453 <dasies11@gmail.com>
2019-06-24 20:10:33 +0200

    initial commit

---
+++ src/main/java/com/store/checkout/service/StoreCheckoutServiceApplication.java
@@ -0,0 +1,13 @@
+package com.store.checkout.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreCheckoutServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreCheckoutServiceApplication.class, args);
+	}
+
+}
