e32a54da36ebb3e5874d5590b004829a818e6dc8
Steampunk1453 <ddda>
2019-06-25 23:10:09 +0200

    get product data

---
+++ src/main/java/com/store/checkout/service/services/BasketProductService.java
@@ -0,0 +1,12 @@
+package com.store.checkout.service.services;
+
+import com.store.checkout.service.repositories.domain.BasketProduct;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+@Validated
+public interface BasketProductService {
+    BasketProduct create(@NotNull(message = "The products for basket cannot be null") @Valid BasketProduct basketProduct);
+}
