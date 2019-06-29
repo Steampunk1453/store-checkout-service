e110a0ab18c2f83089212c8ed6799b72856b2da1
Steampunk1453 <ddda>
2019-06-30 00:25:41 +0200

    exposes endpoints to receive basket data

---
+++ src/main/java/com/store/checkout/service/controllers/BasketController.java
@@ -0,0 +1,53 @@
+package com.store.checkout.service.controllers;
+
+import com.store.checkout.service.dtos.OrderRequest;
+import com.store.checkout.service.repositories.domain.Basket;
+import com.store.checkout.service.services.BasketService;
+import com.store.checkout.service.services.OrderService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.constraints.NotNull;
+
+@RestController
+@RequestMapping("/api/baskets")
+public class BasketController {
+
+    private final BasketService basketService;
+    private final OrderService orderService;
+
+    public BasketController(BasketService basketService, OrderService orderService) {
+        this.basketService = basketService;
+        this.orderService = orderService;
+    }
+
+    @GetMapping(path = "/amounts/{basketId}")
+    @ResponseStatus(HttpStatus.OK)
+    public @NotNull Double getAmount(@PathVariable("basketId") long basketId) {
+        return basketService.getTotalAmount(basketId);
+    }
+
+    @PostMapping
+    public ResponseEntity<Basket> create(@RequestBody OrderRequest orderRequest) {
+        Basket basket = orderService.saveBasket(orderRequest);
+        String uri = ServletUriComponentsBuilder
+                .fromCurrentServletMapping()
+                .path("/baskets")
+                .buildAndExpand(basket.getId())
+                .toString();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", uri);
+
+        return new ResponseEntity<>(basket, headers, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "/{basketId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable("basketId") long basketId) {
+        basketService.delete(basketId);
+    }
+
+}
