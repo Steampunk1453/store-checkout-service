1f6ace1b8dc0485dbdfca97ff075403805a227b0
Steampunk1453 <ddda>
2019-06-30 00:20:47 +0200

    add new methods

--- src/main/java/com/store/checkout/service/services/BasketService.java
+++ src/main/java/com/store/checkout/service/services/BasketService.java
@@ -10,8 +10,9 @@ import javax.validation.constraints.NotNull;
 @Validated
 public interface BasketService {
 
-    @NotNull Iterable<Basket> getAllBaskets();
     Basket create(@NotNull(message = "Basket cannot be null") @Valid Basket basket);
     void update(@NotNull(message = "Basket cannot be null") @Valid Basket basket);
+    @NotNull Double getTotalAmount(long basketId);
+    void delete(long basketId);
 
 }
