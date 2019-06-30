75da0571dbe8d12e3f5949239a46976e7f2c82e6
Steampunk1453 <ddda>
2019-06-30 14:55:00 +0200

    remove unused methods

--- src/main/java/com/store/checkout/service/services/ProductService.java
+++ src/main/java/com/store/checkout/service/services/ProductService.java
@@ -3,12 +3,7 @@ package com.store.checkout.service.services;
 import com.store.checkout.service.repositories.domain.Product;
 
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 
 public interface ProductService {
-
-    @NotNull Iterable<Product> getAllProducts();
     Product getProduct(@Min(value = 1L, message = "Invalid product ID") long id);
-    Product save(Product product);
-
 }
