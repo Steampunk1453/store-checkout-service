cf19b2a896171231fa5e8bf6fa1ed3033a3cef2d
david <david>
2020-02-04 20:21:18 +0100

    add new endpoint to save products

---
+++ src/main/java/com/store/checkout/service/controllers/ProductController.java
@@ -0,0 +1,40 @@
+package com.store.checkout.service.controllers;
+
+import com.store.checkout.service.domain.Basket;
+import com.store.checkout.service.domain.Product;
+import com.store.checkout.service.services.ProductService;
+import com.store.checkout.service.services.dtos.OrderRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/products")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public synchronized ResponseEntity<Product> save(@RequestBody Product product) {
+        productService.save(product);
+        String uri = ServletUriComponentsBuilder
+                .fromCurrentServletMapping()
+                .path("/products")
+                .buildAndExpand(product.getId())
+                .toString();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", uri);
+
+        return new ResponseEntity<>(product, headers, HttpStatus.CREATED);
+    }
+
+}
